c2f72e72e132b0be4a767c4f54bc61be68e68421
ErenyThrwat27 <ereny.thrwat27@gmail.com>
2025-02-16 22:25:16 +0200

    show trips

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Trip;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,32 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        $faker = Faker::create();
+
+        // Insert several trips with fake data
+        Trip::create([
+            'name' => 'Trip to Bali',
+            'coast' => 2000.00,
+            'image' => 'bali-trip.jpg',
+            'number_of_days' => 7,
+        ]);
+
+        Trip::create([
+            'name' => 'Trip to Paris',
+            'coast' => 3000.00,
+            'image' => 'paris-trip.jpg',
+            'number_of_days' => 5,
+        ]);
+
+        Trip::create([
+            'name' => $faker->city, // Random city as trip name
+            'coast' => $faker->randomFloat(2, 1000, 5000), // Random cost
+            'image' => $faker->imageUrl(), // Random image URL
+            'number_of_days' => $faker->numberBetween(5, 15), // Random number of days
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Trips\ShowTripController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+//Show  all trpis
+Route::get('/showtrips', [ShowTripController::class, 'index']);
