e5685e1bf1feb9e62da7aca8a9964e3dc7fe6d95
ErenyThrwat27 <ereny.thrwat27@gmail.com>
2025-02-24 22:25:18 +0200

    add

--- database/seeders/TestimonialSeeder.php
+++ database/seeders/TestimonialSeeder.php
@@ -38,6 +38,7 @@ class TestimonialSeeder extends Seeder
             'updated_at' => now(),
         ]
     ]);
+    
 
     }
 }
--- database/seeders/TripSeeder.php
+++ database/seeders/TripSeeder.php
@@ -13,7 +13,6 @@ class TripSeeder extends Seeder
      */
     public function run(): void
     {
-
         DB::table('trips')->insert([
             [
                 'name' => 'الرياض',
