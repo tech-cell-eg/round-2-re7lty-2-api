7277d4b5978ebc54e95b61dedd350af34c88bd23
ErenyThrwat27 <ereny.thrwat27@gmail.com>
2025-02-26 19:32:54 +0200

    change database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,7 @@ namespace Database\Seeders;
 use App\Models\OurService;
 use App\Models\Testimonial;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 use App\Models\Trip;
 
 class DatabaseSeeder extends Seeder
@@ -17,39 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $faker = Faker::create();
-
-        // Insert several trips with fake data
-        Trip::create([
-            'name' => 'Trip to Bali',
-            'coast' => 2000.00,
-            'image' => 'bali-trip.jpg',
-            'number_of_days' => 7,
-        ]);
-
-        Trip::create([
-            'name' => 'Trip to Paris',
-            'coast' => 3000.00,
-            'image' => 'paris-trip.jpg',
-            'number_of_days' => 5,
-        ]);
-
-        Trip::create([
-            'name' => $faker->city,
-            'coast' => $faker->randomFloat(2, 1000, 5000),
-            'image' => $faker->imageUrl(),
-            'number_of_days' => $faker->numberBetween(5, 15),
-        ]);
+        
+        $this->call(TripSeeder::class);
         $this->call(PlanSeeder::class);
         $this->call(TestimonialSeeder::class);
         $this->call(OurServiceSeeder::class);
-
     }
-
 }
